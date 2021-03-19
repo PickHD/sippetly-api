79ce1f67a75ef0de0dd93fdfe906835f536b37a4
Taufik Januar <taufikjanuar35@gmail.com>
2021-03-19 16:32:34 +0700

    Fix Tabs

--- models/Quote.go
+++ models/Quote.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Quote struct{
-	Id          int		 `json:"id"`
-	QuoteName		string `json:"quoteName" binding:"required"`
-	CreatedBy		string `json:"createdBy" binding:"required"`
-	CreatedAt		time.Time `json:"createdAt"`
+	Id          	int		 	`json:"id"`
+	QuoteName		string 		`json:"quoteName" binding:"required"`
+	CreatedBy		string 		`json:"createdBy" binding:"required"`
+	CreatedAt		time.Time 	`json:"createdAt"`
 	UpdatedAt		time.Time	`json:"updatedAt"`
-}
+}	
