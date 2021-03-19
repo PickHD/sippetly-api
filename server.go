889448948c64d5868315d05ca474f54fca4b394c
PickHD <taufikjanuar35@gmail.com>
2021-03-19 15:52:43 +0700

    Add server

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"sippetly-api/db"
+	"sippetly-api/routes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+  godotenv.Load()
+	
+	go func() {
+		db.Init()
+	}()
+
+	r:=gin.Default()
+	routes.QuoteRoutes(r)
+
+	if err:=r.Run();err!=nil{
+		log.Fatalf("Server Failed with '%s'\n",err)
+	}
+
+}
